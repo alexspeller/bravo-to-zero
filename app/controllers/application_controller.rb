b3edcbd8930b8a244af42408e2facdda130ded66
Alex Speller <alex@alexspeller.com>
2014-10-19 12:21:46 +0100

    Force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: Rails.env.production?, host:'vast-depths-1206.herokuapp.com'
 
   private
 
